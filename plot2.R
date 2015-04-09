c0897ea802c32814afa197383539a15618ca38cd
R <r.robertv@gmail.com>
2015-04-10 00:25:50 +0300

    added plot2 files

---
+++ plot2.R
@@ -0,0 +1,22 @@
+# plot 2
+
+# the file below is cleaned up
+data <- read.csv("data/hpc.txt", sep=";", header=TRUE,
+                 na.strings=c("?"),
+                 colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric",
+                              "numeric", "numeric", "numeric"))
+
+# create a new datetime field
+data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), '%d/%m/%Y %H:%M:%S')
+names(data)
+head(data)
+class(data$DateTime)
+
+png(filename="plot2.png", width=504, height=504)
+par(bg="white", fg="black")
+with(data, plot(DateTime, Global_active_power, type="l", main=""
+                , ylab="Global Active Power (kilowatts)"
+                , xlab=""))
+dev.off()
+
+
